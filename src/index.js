acd0c77311d0e434ee7df12753fd17a89474273b
Mizael Paredes Vielma <mizael.paredes@global66.com>
2021-07-09 22:10:28 -0400

    first commit to configurate webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  "Here we are with React again 🚀",
+  document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    filename: "app.bundle.js",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
